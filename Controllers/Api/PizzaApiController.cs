69c76e2bad85d43c6383e32ebc052dfd3aff40cd
Nothung429 <frank.gatto96.fg@gmail.com>
2022-10-14 18:30:54 +0200

    add api frontend view

---
+++ Controllers/Api/PizzaApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_crud_webapi.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzaApiController : ControllerBase
+    {
+        PizzeriaContext context;
+
+        public PizzaApiController()
+        {
+            context = new PizzeriaContext();
+        }
+
+        [HttpGet]
+        public ActionResult GetPizza()
+        {
+            List<Pizza> pizza = context.Pizza.ToList();
+
+            return Ok(pizza);
+        }
+    }
+}
