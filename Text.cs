1de36d094458fe55f5c394d73fa1ca293ba5c7c5
Nothung429 <frank.gatto96.fg@gmail.com>
2022-10-17 14:17:43 +0200

    add text

--- Text.cs
+++ Text.cs
@@ -39,3 +39,10 @@
 //Abbiamo studiato cosa sono le WebApi...è il momento di mettere in pratica quello che abbiamo imparato.
 //Dobbiamo quindi creare un controller Controllers/Api/PizzasController che implementerà le nostre webapi. Deve avere un metodo Get() che restituisca la lista delle pizze che offre la nostra pizzeria.
 //Dobbiamo poi modificare la nostra homepage Controller/HomeController, che richiamerà utilizzando axios il metodo Get() appena creato e che mostrerà i dati ottenuti tramite codice javascript.
+
+
+//PARTE 6
+
+//La nostra pizzeria è quasi conclusa...ma...come fanno i nostri clienti a trovare una pizza se ne abbiamo tante nel nostro menu? Dobbiamo dargli la possibilità di ricercarle con un filtro.
+//Modifichiamo quindi la pagina index, aggiungendo un text input di ricerca. Ogni volta che viene scritto qualcosa, il sistema deve interrogare le nostre WebApi chiedendo indietro le pizze che contengono nel titolo quello che ha chiesto l’utente, e mostriamo in pagina i risultati filtrati.
+//Al click della card di una pizza, dobbiamo essere portati a una pagina di dettaglio. I dati da mostrare di questa pagina devono essere richiesti alle nostra webapi tramite l’id della pizza e mostrati in pagina usando il javascript.
