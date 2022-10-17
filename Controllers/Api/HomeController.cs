7a4b1ad099e59ff6a72a9ef453e6d87b8b058510
Nothung429 <frank.gatto96.fg@gmail.com>
2022-10-17 17:07:27 +0200

    add searchbar

---
+++ Controllers/Api/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+
+namespace la_mia_pizzeria_crud_webapi.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        PizzeriaContext context;
+        public HomeController()
+        {
+            context = new PizzeriaContext();
+        }
+
+        [HttpGet]
+        public IActionResult GetPizza(string? name)
+        {
+            IQueryable<Pizza> PizzaDB;
+
+            if (name != null)
+            {
+                PizzaDB = context.Pizza.Where(pizza => pizza.Name.ToLower().Contains(name.ToLower()));
+            }
+            else
+            {
+                PizzaDB = context.Pizza;
+            }
+
+            return Ok(PizzaDB.ToList<Pizza>());
+        }
+    }
+}
